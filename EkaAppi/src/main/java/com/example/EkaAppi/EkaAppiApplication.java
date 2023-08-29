3b017c3af291a0cf349572074071034284eb4d4d
MantMar <143549634+MantMar@users.noreply.github.com>
2023-08-29 23:35:26 +0300

    Add files via upload

---
+++ EkaAppi/src/main/java/com/example/EkaAppi/EkaAppiApplication.java
@@ -0,0 +1,13 @@
+package com.example.EkaAppi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkaAppiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkaAppiApplication.class, args);
+	}
+
+}
